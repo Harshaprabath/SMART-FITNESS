a3b33d7703043f38f884cbaa7e72da94b9b5a53b
Harshaprabath <prabhathhemasiri@gmail.com>
2022-03-07 20:27:43 +0530

    create project

--- src/main/java/com/smartFitness/itpm/SmartFitnessApplication.java
+++ src/main/java/com/smartFitness/itpm/SmartFitnessApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartFitnessApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SmartFitnessApplication.class, args);
 	}
