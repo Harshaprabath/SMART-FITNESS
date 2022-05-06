6d85861dfdd39d502ac9c0208b3778c3d1cf1a2a
sajitheranda05 <sajitheranda05@gmail.com>
2022-05-06 22:36:55 +0530

    Revert "exercise page added"
    This reverts commit 0bd3435c

--- src/main/java/com/smartFitness/itpm/IServices/IWeightService.java
+++ src/main/java/com/smartFitness/itpm/IServices/IWeightService.java
@@ -1,15 +1,2 @@
-package com.smartFitness.itpm.IServices;
-
-import com.smartFitness.itpm.Models.WeightGain;
-import com.smartFitness.itpm.ViewModel.Response;
-
-import java.util.List;
-
-public interface IWeightService {
-
-    public List<WeightGain> findAllWeightPlans();
-    public Response saveWeightPlan(WeightGain weightGain);
-    public Response deleteWeightPlan(Integer weightGainId);
-    public WeightGain findById(Integer weightGainId);
-
+package com.smartFitness.itpm.IServices;public interface IWeightService {
 }
--- src/main/java/com/smartFitness/itpm/Repositories/WeightGainRepository.java
+++ src/main/java/com/smartFitness/itpm/Repositories/WeightGainRepository.java
@@ -1,7 +1,2 @@
-package com.smartFitness.itpm.Repositories;
-
-import com.smartFitness.itpm.Models.WeightGain;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface WeightGainRepository extends CrudRepository<WeightGain, Integer> { }
+package com.smartFitness.itpm.Repositories;public interface WeightGainRepository {
+}
