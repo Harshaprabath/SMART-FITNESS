af37a65aaba18ee43c29e821f1758900c280eeb1
sajitheranda05 <sajitheranda05@gmail.com>
2022-05-08 09:41:27 +0530

    back end work

--- src/main/java/com/smartFitness/itpm/IServices/IWeightScheduleService.java
+++ src/main/java/com/smartFitness/itpm/IServices/IWeightScheduleService.java
@@ -1,2 +1,23 @@
-package com.smartFitness.itpm.IServices;public interface IWeightScheduleService {
+package com.smartFitness.itpm.IServices;
+
+import com.smartFitness.itpm.Models.WeightSchedule;
+import com.smartFitness.itpm.ViewModel.Response;
+
+import java.util.List;
+
+public interface IWeightScheduleService {
+
+    public List<WeightSchedule> findAllWeightSchedules();
+
+    public Response saveWeightSchedule(WeightSchedule weightSchedule);
+
+    public Response deleteWeightSchedule(Integer id);
+
+    public WeightSchedule findById(Integer id);
+
 }
+
+
+
+
+
--- src/main/java/com/smartFitness/itpm/Models/WeightSchedule.java
+++ src/main/java/com/smartFitness/itpm/Models/WeightSchedule.java
@@ -1,2 +1,91 @@
-package com.smartFitness.itpm.Models;public class WeightSchedule {
+package com.smartFitness.itpm.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "weight_schedules")
+public class WeightSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "gymName", nullable = false)
+    private String gymName;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "serviceCharge", nullable = false)
+    private double serviceCharge;
+
+    @Column(name = "isActive", nullable = false)
+    private Boolean isActive;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGymName() {
+        return gymName;
+    }
+
+    public void setGymName(String gymName) {
+        this.gymName = gymName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getServiceCharge() {
+        return serviceCharge;
+    }
+
+    public void setServiceCharge(double serviceCharge) {
+        this.serviceCharge = serviceCharge;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 }
+
+
+
+
