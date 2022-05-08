6701e158e71bcea0981571e0110da8c14463c382
sajitheranda05 <sajitheranda05@gmail.com>
2022-05-08 17:34:51 +0530

    bkend model cls added to the function of event

---
+++ src/main/java/com/smartFitness/itpm/Models/weightEvent.java
@@ -0,0 +1,64 @@
+package com.smartFitness.itpm.Models;
+
+/*import javax.persistence.*;
+
+@Entity
+@Table(name = "weight_event")
+public class weightEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "entranceFee", nullable = false)
+    private Integer entranceFee;
+
+    @Column(name = "entrants", nullable = false)
+    private Integer entrants;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getEntranceFee() {
+        return entranceFee;
+    }
+
+    public void setEntranceFee(Integer entranceFee) {
+        this.entranceFee = entranceFee;
+    }
+
+    public Integer getEntrants() {
+        return entrants;
+    }
+
+    public void setEntrants(Integer entrants) {
+        this.entrants = entrants;
+    }
+}*/
