a839999c27da32e6d5592604a9beb4c60923c342
sajitheranda05 <sajitheranda05@gmail.com>
2022-05-08 10:37:36 +0530

    weight schedule back end complete

---
+++ src/main/java/com/smartFitness/itpm/Repositories/WeightScheduleRepository.java
@@ -0,0 +1,6 @@
+package com.smartFitness.itpm.Repositories;
+
+import com.smartFitness.itpm.Models.WeightSchedule;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WeightScheduleRepository extends CrudRepository<WeightSchedule, Integer> { }
